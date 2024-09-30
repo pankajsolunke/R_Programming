f77e2e9b5371129e5e0ecac03583f55e71d1bc72
Pankaj Solunke <94754779+pankajsolunke@users.noreply.github.com>
2024-09-30 01:42:41 -0700

    Vector program

---
+++ Vector_arithmetic[1].R
@@ -0,0 +1,19 @@
+number1 <- c(4,3,8,10.27)
+number2 <- c(5,7,1,27,25)
+
+sum <- number1 + number2
+print(sum)
+
+sorted_number1 <- sort(number1)
+print(sorted_number1)
+
+sorted_number1 <- sort(number1, decreasing = TRUE)
+print(sorted_number1)
+
+fruits <- c("banana", "apple", "cherry", "date")
+sorted_fruits <- sort(fruits, )
+print(sorted_fruits)  
+
+
+
+
